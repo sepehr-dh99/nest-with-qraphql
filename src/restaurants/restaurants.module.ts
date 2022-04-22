91062d6bb5933ffe03858692daed8e7b7a202044
sepehr-dh99 <sepehr.dh99@gmail.com>
2022-04-22 23:56:09 +0430

    try to create a mutation for create a restaurant

---
+++ src/restaurants/restaurants.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RestaurantsResolver } from './restaurants.resolver';
+
+@Module({
+    providers: [
+        RestaurantsResolver
+    ],
+})
+export class RestaurantsModule {}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,19 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantsResolver {
+  @Query(() => [Restaurant])
+  myRestaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation(() => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    console.log(createRestaurantInput);
+    return true;
+  }
+}
