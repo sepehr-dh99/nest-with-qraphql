430893ee1f8fe5873e7653e2a2ef93126985acea
nima nekouei <nekoueinima@protonmail.com>
2023-02-21 19:30:35 +0330

    [Feat]: add next-pwa and its configurations

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@
 
 const path = require("path");
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "src", "styles")],
   },
+  // minimumCacheTTL: 60, // Set the minimum cache TTL to 60 seconds
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
