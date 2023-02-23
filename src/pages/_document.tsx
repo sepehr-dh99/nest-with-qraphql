6044797bcc1f5329227da0be29f0008d60ff86ce
nima nekouei <nekoueinima@protonmail.com>
2023-02-23 16:54:40 +0330

    [Fix]: fix default loading state of button and change language of _document to farsi

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='fa'>
       <Head>
         <link rel='manifest' href='/manifest.json' />
         <link rel='apple-touch-icon' href='/logo.svg'></link>
